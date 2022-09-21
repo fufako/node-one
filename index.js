56ad9f150ef60adcb82843142e8f79c1d95a13ce
fufako <patryko.mikiciuk@gmail.com>
2022-09-21 12:55:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 const http = require("http")
+const path = require("path")
+const fs = require("fs")
 
 const server = http.createServer((req, res) => {
-  res.end("<h1>Home</h1>")
+  //Build file path
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  )
+
+  //Extension of file
+  let extname = path.extname(filePath)
+
+  //Initial content type
+  let contentType = "text/html"
+
+  //Check ext and set content type
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript"
+      break
+    case ".css":
+      contentType = "text/css"
+      break
+    case ".json":
+      contentType = "application/json"
+      break
+    case ".png":
+      contentType = "image/png"
+      break
+  }
+
+  //Read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      fs.readFile(
+        path.join(__dirname, "public", "404.html"),
+        (err, content) => {
+          res.writeHead(200, { "Content-Type": "text/html" })
+          res.end(content, "utf8")
+        }
+      )
+    } else {
+      res.writeHead(200, { "Content-Type": contentType })
+      res.end(content, "utf8")
+    }
+  })
 })
 
 const PORT = process.env.PORT || 5000
